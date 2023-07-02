2d17458a6e618709b2f67462597cb1a394720ae4
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-07-02 22:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function findMajorityElement(nums) {
+  let candidate;
+  let count = 0;
+
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+      count = 1;
+    } else if (candidate === num) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return candidate;
+}
